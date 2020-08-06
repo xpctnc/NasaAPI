6a4f9647ef0cf1f7d487eea540bfed0ebdf684a1
Adrian Prusik <xpctnc@gmail.com>
2020-08-06 22:20:27 +0200

    Add simple fetch handling

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
+
 const searchInput = document.querySelector('.searchInput')
 const searchInputBorder = document.querySelector('.searchInputBorder')
+const searchButton = document.querySelector('.searchButton')
 
 searchInput.addEventListener('keyup', () => {
     searchInput.value != '' ? searchInputBorder.classList.add('active') : searchInputBorder.classList.remove('active')
+
+})
+
+const api = 'https://images-api.nasa.gov/'
+
+searchButton.addEventListener('click', () => {
+    const query = searchInput.value
+    fetch(`${api}search?q=${query}&media_type=image`).then(res => res.json()).then(data => console.log(data))
 })
