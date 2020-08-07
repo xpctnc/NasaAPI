c62858f7ce240d084f23e3ac619e9e2d6f87672a
Adrian Prusik <xpctnc@gmail.com>
2020-08-07 15:52:29 +0200

    Add debounce function

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
-
+const searchBox = document.querySelector('.searchBox')
 const searchInput = document.querySelector('.searchInput')
 const searchInputBorder = document.querySelector('.searchInputBorder')
-const searchButton = document.querySelector('.searchButton')
 
 searchInput.addEventListener('keyup', () => {
     searchInput.value != '' ? searchInputBorder.classList.add('active') : searchInputBorder.classList.remove('active')
@@ -10,7 +9,46 @@ searchInput.addEventListener('keyup', () => {
 
 const api = 'https://images-api.nasa.gov/'
 
-searchButton.addEventListener('click', () => {
-    const query = searchInput.value
-    fetch(`${api}search?q=${query}&media_type=image`).then(res => res.json()).then(data => console.log(data))
-})
+const debounce = (fn, delay) => {
+    let timeOutId;
+    return function(...args) {
+        clearTimeout(timeOutId)
+        timeOutId = setTimeout(() => fn(...args), delay);
+    }
+}
+
+searchInput.addEventListener('keyup', debounce(e =>{
+    console.log(searchInput.value.length)
+    searchBox.classList.add('loadingItems')
+    if(searchInput.value.length === 0) {
+        searchInput.blur();
+        searchBox.classList.remove('loadingItems')
+
+    }
+},1500))
+
+// searchInput.addEventListener('keyup', () => {
+//
+//     let loading = true;
+//
+//     const inputValue = () => {
+//         const query = searchInput.value;
+//         console.log(query)
+//         loading = false
+//         //!loading ? searchBox.classList.add('itemsLoaded') : '';
+//     }
+//     //const query = searchInput.value
+//     //console.log(query)
+//     // fetch(`${api}search?q=${query}&media_type=image`)
+//     //     .then(res => res.json())
+//     //     .then(data => {
+//     //         itemsLoaded = true;
+//     //         itemsLoaded ? searchBox.classList.add('itemsLoaded') : '';
+//     //         console.log(data);
+//     //
+//     //     })
+//
+// })
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,10 @@
-const path from '';
+var path = require('path');
 
+module.exports = {
+    mode: 'development',
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'foo.bundle.js'
+    }
+};
