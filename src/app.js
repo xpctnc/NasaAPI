a21b0edf28ec57c60befc4b4c1cdb1d7194e481c
Adrian Prusik <xpctnc@gmail.com>
2020-08-07 22:27:10 +0200

    Add image url fetching

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
+const container = document.querySelector('.container')
 const searchBox = document.querySelector('.searchBox')
 const searchInput = document.querySelector('.searchInput')
 const searchInputBorder = document.querySelector('.searchInputBorder')
+const images = document.querySelector('.results')
+//const imageItem = document.querySelector('.item')
 
 searchInput.addEventListener('keyup', () => {
     searchInput.value != '' ? searchInputBorder.classList.add('active') : searchInputBorder.classList.remove('active')
@@ -18,7 +21,8 @@ const debounce = (fn, delay) => {
 }
 
 searchInput.addEventListener('focus', () => {
-    searchBox.classList.add('moveToTop')
+    searchBox.classList.add('stateTwo')
+    container.classList.add('stateTwo')
 })
 
 searchInput.addEventListener('keyup', debounce(e => {
@@ -32,6 +36,18 @@ searchInput.addEventListener('keyup', debounce(e => {
         .then(response => {
             console.log(response);
             results = response.collection.items
+            console.log(results)
+            for(let i of results) {
+                console.log(i)
+
+                let imageItem = document.createElement('div');
+                imageItem.classList.add('item');
+                imageItem.style.backgroundImage = `url('${i.links[0].href}')`
+                images.appendChild(imageItem);
+
+
+                //imageItem.innerHTML = `<img src="${i.links[0].href}">`
+            }
 
         })
         .catch((err) => console.log(err));
