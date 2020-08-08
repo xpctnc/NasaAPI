3238738632751a10b1f746c7378da92b44337578
Adrian Prusik <xpctnc@gmail.com>
2020-08-08 16:54:08 +0200

    Add responsive grid

--- src/app.js
+++ src/app.js
@@ -1,65 +1,50 @@
-const container = document.querySelector('.container')
-const searchBox = document.querySelector('.searchBox')
+const searchContainer = document.querySelector('.searchContainer')
+const search = document.querySelector('.search')
 const searchInput = document.querySelector('.searchInput')
 const searchInputBorder = document.querySelector('.searchInputBorder')
-const images = document.querySelector('.results')
-//const imageItem = document.querySelector('.item')
+const results = document.querySelector('.results')
 
 searchInput.addEventListener('keyup', () => {
     searchInput.value != '' ? searchInputBorder.classList.add('active') : searchInputBorder.classList.remove('active')
 
 })
 
-const api = 'https://images-api.nasa.gov/'
-
 const debounce = (fn, delay) => {
+
     let timeOutId;
     return function(...args) {
         clearTimeout(timeOutId)
         timeOutId = setTimeout(() => fn(...args), delay);
     }
 }
-
 searchInput.addEventListener('focus', () => {
-    searchBox.classList.add('stateTwo')
-    container.classList.add('stateTwo')
+    search.classList.add('searchActive')
+    searchContainer.classList.add('searchActive')
 })
 
+const api = 'https://images-api.nasa.gov/'
+searchInput.focus();
 searchInput.addEventListener('keyup', debounce(e => {
     let state = 1;
 
-
-    let results = new Array();
+    let resultsArr = new Array();
 
     fetch(`${api}search?q=${searchInput.value}&media_type=image`)
-        .then(res => res.json())
+        .then(response => response.json())
         .then(response => {
             console.log(response);
-            results = response.collection.items
-            console.log(results)
-            for(let i of results) {
-                console.log(i)
+            resultsArr = response.collection.items
+            console.log(resultsArr)
+            resultsArr.forEach(item => {
+                let imageItem = document.createElement('div')
+                imageItem.classList.add('item')
+                imageItem.style.backgroundImage = `url('${item.links[0].href}')`
+                //images.appendChild(imageItem)
+                console.log(`${item.links[0].href}`)
+            })
 
-                let imageItem = document.createElement('div');
-                imageItem.classList.add('item');
-                imageItem.style.backgroundImage = `url('${i.links[0].href}')`
-                images.appendChild(imageItem);
-
-
-                //imageItem.innerHTML = `<img src="${i.links[0].href}">`
-            }
 
         })
         .catch((err) => console.log(err));
 
 },1000))
-
-
-// fetch(`${api}search?q=${query}&media_type=image`)
-//     //     .then(res => res.json())
-//     //     .then(data => {
-//     //         itemsLoaded = true;
-//     //         itemsLoaded ? searchBox.classList.add('itemsLoaded') : '';
-//     //         console.log(data);
-//     //
-//     //     })
