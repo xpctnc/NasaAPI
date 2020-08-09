0ff435277e45a87931fc62b39f0b1e2f5f84ff15
Adrian Prusik <xpctnc@gmail.com>
2020-08-09 17:29:18 +0200

    Fixed loader and no results info

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const searchContainer = document.querySelector('.search-container')
 const search = document.querySelector('.search')
 const searchInput = document.querySelector('.search-input')
 const searchInputBorder = document.querySelector('.search-input-border')
-const loader = document.querySelector('.lds')
+const loader = document.querySelector('.loader')
 const results = document.querySelector('.results')
 
 searchInput.addEventListener('keyup', () => {
@@ -37,8 +37,6 @@ const api = 'https://images-api.nasa.gov/'
 
 searchInput.addEventListener('keyup', debounce(e => {
 
-
-
     let resultsArr = new Array();
 
     results.childNodes.length > 0 && searchInput.value.length > 0 ?  removeAllChildNodes(results) : '';
@@ -47,44 +45,30 @@ searchInput.addEventListener('keyup', debounce(e => {
 
         loader.classList.add('loading');
 
-
         fetch(`${api}search?q=${searchInput.value}&media_type=image`)
             .then(response => response.json())
             .then(response => {
 
                 resultsArr = response.collection.items
 
-
-
                 resultsArr.forEach(item => {
-
                     const imageItem = document.createElement('div')
                     imageItem.classList.add('item')
                     imageItem.style.backgroundImage = `url('${item.links[0].href}')`
                     results.appendChild(imageItem)
-
-
                 })
-
-
 
                 loader.classList.remove('loading')
 
                 results.parentElement.classList.add('items-loaded');
 
-                if(resultsArr.length === 0){
-                    loader.innerHTML = '<span class="no-results">no results</span>';
-                    results.parentElement.classList.remove('items-loaded');
-
+                if(resultsArr.length === 0) {
+                    results.innerHTML = '<span class="no-results">no results</span>';
+                    results.parentNode.classList.remove('items-loaded');
                 }
-
-
-
 
             })
             .catch((err) => console.log(err));
-
-
 
     }
 
